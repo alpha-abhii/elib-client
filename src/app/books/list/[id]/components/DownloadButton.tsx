18f993000940456675bc9319912b879432c50540
alpha-abhii <alpha20abhi@gmail.com>
2024-06-05 22:17:02 +0530

    add download button

--- src/app/books/list/[id]/components/DownloadButton.tsx
+++ src/app/books/list/[id]/components/DownloadButton.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
-const DownloadButton = () => {
+const DownloadButton = ({ fileLink }: { fileLink: string }) => {
+    const handleDownload = () => {
+        window.open(fileLink, '_blank');
+    };
+
     return (
-        <div>DownloadButton</div>
-    )
-}
+        <button
+            onClick={handleDownload}
+            className="mt-10 h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700">
+            Download the book
+        </button>
+    );
+};
 
-export default DownloadButton
+export default DownloadButton;
