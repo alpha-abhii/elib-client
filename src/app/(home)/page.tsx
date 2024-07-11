f645416b94f2320d9711f05b679deabab3a42f9a
alpha-abhii <alpha20abhi@gmail.com>
2024-07-12 01:34:56 +0530

    removed unnessary import

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Banner from "./components/Banner";
 import BookList from "./components/BookList";
 import { Suspense } from "react";
