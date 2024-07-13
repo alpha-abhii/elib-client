ca5138a561468a8b495a0c9dcda5006ec9fef265
alpha-abhii <alpha20abhi@gmail.com>
2024-07-13 07:05:49 +0530

    fixed the issue of backend url

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+
 import Banner from "./components/Banner";
 import BookList from "./components/BookList";
 import { Suspense } from "react";
@@ -7,11 +7,6 @@ import Loading from "@/components/Loading";
 // export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  // // const response = await fetch(`${process.env.BACKEND_URL}/books/list`,{ cache: 'no-store' });
-
-  // // console.log("BookList:: ",response);
-  console.log("Hello");
-  console.log(`${process.env.NEXT_PUBLIC_BACKEND_URL}`)
 
   return (
     <>
