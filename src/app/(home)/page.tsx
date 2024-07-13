a654e940ad8f03ebeeae88dc9e31a64ee99d020e
alpha-abhii <alpha20abhi@gmail.com>
2024-07-13 08:06:53 +0530

    test-01

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,6 +8,16 @@ import Loading from "@/components/Loading";
 
 export default async function Home() {
 
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/books/list`,{ cache: 'no-store' });
+
+    console.log("BookList:: ",response);
+
+    if (!response.ok) {
+        throw new Error("An error occurred while fetching the data");
+    }
+    const books = await response.json();
+    console.log(books);
+
   return (
     <>
       <Banner/>
